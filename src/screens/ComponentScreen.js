c34139bc8e62a17e9603e47ea2a9034a97f80fb6
rogerprz <rogerprz@github.com>
2021-02-17 21:25:10 -0800

    configure default settings

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-
+import React from "react";
+import { Text, StyleSheet } from "react-native";
 
 const ComponentScreen = () => {
-  return <Text style={styles.text}>Component test screen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Component test screen</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
-    textAlign: 'center'
-  }
+    textAlign: "center",
+  },
 });
 
 export default ComponentScreen;
